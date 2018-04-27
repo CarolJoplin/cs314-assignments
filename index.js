b82bafa2adb89fa0626ee73656f54e793b4b923d
CarolMaria <35873776+CarolMaria@users.noreply.github.com>
2018-04-27 09:37:45 -0700

    albums toggle and todos toggle functional

--- index.js
+++ index.js
@@ -6,35 +6,34 @@
 
 $.ajax({}).done().fail().always();
 
+$.ajax({
+	url: 'https://jsonplaceholder.typicode.com/users/',
+   dataType : "json",
+})
+	
+.done(function( response ) {
+	displayUserData(response);
+})
+	
+	
+.fail(function( status, errorThrown ) {
+	console.log("Error: ", errorThrown);
+	console.log("Status: ", status);
+})
+	
+.always(function() {
+	console.log("Request is complete!");
+})
 
-//$('#buttonClickUser').click(() => {
-	$.ajax({
-		url: 'https://jsonplaceholder.typicode.com/users/',
-        dataType : "json",
-	})
-	
-    .done(function( response ) {
-		displayUserData(response);
-	})
-	
-	
-	.fail(function( status, errorThrown ) {
-		console.log("Error: ", errorThrown);
-		console.log("Status: ", status);
-	})
-	
-	.always(function() {
-		console.log("Request is complete!");
-	})
-//});
 
 //displayUserData
 //name
 //email
 //company
 function displayUserData(data) {
-    //console.log(data);
+
     for(let user of data) {
+
         let userContainer = document.createElement('div');
         userContainer.id = user.id;
 		userContainer.className = 'user-container';
@@ -67,27 +66,24 @@ function displayUserData(data) {
 
 		$(`#${user.id}`).append(albumsButton);
 		$(`#${user.id}`).append(todosButton);
-        
+		
+		// toggle between albums and todos
+
         // albums button event listener
         $(`#albums-button-${user.id}`).click((event) => {
-			//albumsButtonClick(event);
-			console.log(user.id);
-			albumsButtonClick(user.id);
+			albumsButtonClick(event, user.id);
 		});
 		
 		// todos button event listener
 		$(`#todos-button-${user.id}`).click((event) => {
-			//albumsButtonClick(event);
-			todosButtonClick(user.id);
+			todosButtonClick(event, user.id);
         });
 		
     }
 }
 
 // if you know todos is clicked, hide albums and visa versa
-function albumsButtonClick(userId) {
-	//console.log(event);
-	console.log(userId);
+function albumsButtonClick(event, userId) {
 	
 	if(event.target.dataset.loaded) 
 		// grab comments' container and toggle it off
@@ -95,13 +91,11 @@ function albumsButtonClick(userId) {
 		
 	else // all this:
 		$.ajax({
-			url: `https://jsonplaceholder.typicode.com/albums/`,
+			url: `https://jsonplaceholder.typicode.com/users/${userId}/albums/`,
 			dataType : "json",
 		})
 	
 		.done(function( response ) {
-			//albumsLoaded = true;
-			//console.log("data-loaded, true");
 			// adding an attribute to the button when the data has loaded
 			$(event.target).attr('data-loaded', 'true');
 			displayAlbums(response);
@@ -119,24 +113,11 @@ function albumsButtonClick(userId) {
 }
 
 function displayAlbums(albums) {
-	// console.log(album.userId);
-	// console.log(album.id);
-	// console.log(album.title);
-	// console.log(album);
 	let outerAlbumsContainer = document.createElement('div');
-	//console.log(`outer-container-${album.userId}`);
-	//outerAlbumsContainer.id = 'outer-albums-container-' + albums[0].userId;
 	outerAlbumsContainer.id = `outer-albums-container-${albums[0].userId}`;
-	//console.log(`outerContainer: ${outerContainer.id}`);
-	$(`${albums[0].userId}`).append(outerAlbumsContainer);
+	$(`#${albums[0].userId}`).append(outerAlbumsContainer);
 	
 	for (let album of albums) {
-		//console.log(comment);
-
-		// let outerAlbumsContainer = document.createElement('div');
-		// //outerAlbumsContainer.id = 'outer-albums-container-' + albums[0].userId;
-		// outerAlbumsContainer.id = 'outer-albums-container-' + albums[0].userId;
-		// $(`${albums[0].userId}`).append(outerAlbumsContainer);
 		
 		let albumContainer = document.createElement('div');
 		albumContainer.id = 'album-container-' + album.id;
@@ -147,28 +128,25 @@ function displayAlbums(albums) {
 		albumTitle.className = 'album-container';
 		albumTitle.innerHTML = album.title;
 
-		$(`#${album.userId}`).append(albumContainer);
+		$(outerAlbumsContainer).append(albumContainer);
 		$(`#album-container-${album.id}`).append(albumTitle);
 		$(`${album.userId}`).append(outerAlbumsContainer);
-		console.log(`${album.userId} added to outerAlbumsContainer`);
-		//$(`#comment-container-${comment.id}`).append(commentBody);
 	
 	}
 }
 
+function todosButtonClick(event, userId) {
 
-function todosButtonClick(userId) {
 	if(event.target.dataset.loaded)
 		// grab todos container and toggle it off
 		$(`#outer-todos-container-${userId}`).toggle();
 	else // all this:
 	$.ajax({
-		url: `https://jsonplaceholder.typicode.com/todos/`,
+		url: `https://jsonplaceholder.typicode.com/users/${userId}/todos/`,
 		dataType : "json",
 	})
 	
 	.done(function( response ) {
-		//todosLoaded = true;
 		$(event.target).attr('data-loaded', 'true');
 		displayTodos(response);
 	})
@@ -184,17 +162,12 @@ function todosButtonClick(userId) {
 }
 
 function displayTodos(todos) {
-	//console.log(todos);
+	
 	let outerTodosContainer = document.createElement('div');
-	outerTodosContainer.id = 'outer-todos-container-' + todos[0].userId;
-	$(`${todos[0].userId}`).append(outerTodosContainer);
+	outerTodosContainer.id = `outer-todos-container-${todos[0].userId}`;
+	$(`#${todos[0].userId}`).append(outerTodosContainer);
 	
 	for (let todo of todos) {
-		//console.log(comment);
-
-		// let outerTodosContainer = document.createElement('div');
-		// outerTodosContainer.id = 'outer-todos-container-' + todos[0].userId;
-		// $(`${todos[0].userId}`).append(outerTodosContainer);
 		
 		let todoContainer = document.createElement('div');
 		todoContainer.id = 'todo-container-' + todo.id;
@@ -204,9 +177,9 @@ function displayTodos(todos) {
 		todoTitle.className = 'todo-container';
 		todoTitle.innerHTML = todo.title;
 
-		$(`#${todo.userId}`).append(todoContainer);
+		$(outerTodosContainer).append(todoContainer);
 		$(`#todo-container-${todo.id}`).append(todoTitle);
-		$(`${todo.userId}`).append(outerAlbumsContainer);
+		$(`${todo.userId}`).append(outerTodosContainer);
 	
 	}
 }
